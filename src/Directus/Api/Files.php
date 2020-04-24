650d8af51616599bbfc9e23550e25fd6a12f57a7
Andrew Rhyand <andrew.rhyand@gmail.com>
2020-04-24 09:32:54 -0700

    complete files endpoints

--- src/Directus/Api/Files.php
+++ src/Directus/Api/Files.php
@@ -6,6 +6,16 @@ use BoxyBird\Directus\Directus\ApiWrapperBase;
 
 class Files extends ApiWrapperBase
 {
+    public function list(array $params = [], string $jwt = '')
+    {
+        return $this->handleApiRequest('GET', '/files', $params, $jwt);
+    }
+
+    public function retrieve(int $id, array $params = [], string $jwt = '')
+    {
+        return $this->handleApiRequest('GET', "/files/{$id}", $params, $jwt);
+    }
+
     public function create(array $params, string $jwt = '')
     {
         return $this->handleApiRequest('POST', '/files', $params, $jwt);
@@ -14,5 +24,20 @@ class Files extends ApiWrapperBase
     public function update(int $id, array $params, string $jwt = '')
     {
         return $this->handleApiRequest('PATCH', "/files/{$id}", $params, $jwt);
+    }
+
+    public function delete(int $id, string $jwt = '')
+    {
+        return $this->handleApiRequest('DELETE', "/files/{$id}", [], $jwt);
+    }
+
+    public function listRevisions(int $id, array $params = [], string $jwt = '')
+    {
+        return $this->handleApiRequest('GET', "/files/{$id}/revisions", $params, $jwt);
+    }
+
+    public function retrieveRevision(int $id, int $offset, array $params = [], string $jwt = '')
+    {
+        return $this->handleApiRequest('GET', "/files/{$id}/revisions/{$offset}", $params, $jwt);
     }
 }
